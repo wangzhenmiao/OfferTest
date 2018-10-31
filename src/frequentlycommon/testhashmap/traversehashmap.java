d9ed041ffc6f1c11642f299db180a4b648e1e2be
wang.zhen <vq.momomiao>
2018-10-31 14:12:32 +0800

    完善hashmap的遍历

--- src/frequentlycommon/testhashmap/traversehashmap.java
+++ src/frequentlycommon/testhashmap/traversehashmap.java
@@ -1,7 +1,12 @@
 package frequentlycommon.testhashmap;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
+/**
+ * 遍历hashmap
+ */
 public class traversehashmap {
 
 	public static void main(String[] args) {
@@ -10,17 +15,32 @@ public class traversehashmap {
 		map.put("111", "aaa");
 		map.put("222","bbb");
 		map.put("333", "ccc");
-		
-		System.out.println("使用Map.keyset()遍历key和value");
-		for(String key:map.keySet())
-		{
-			System.out.println("key:"+key);
+
+		//第一种方式，普遍使用，使用Map.keySet()遍历key和value
+		System.out.println("使用Map.keySet()遍历key和value");
+		for(String key:map.keySet()){
+			System.out.println("key:"+key+",value:"+map.get(key));
 		}
-		for(String value:map.values()){
-			System.out.println("value:"+value);
-		}
-		
+
+		//第二种方式,使用Map.entrySet(),使用Iterator遍历key和value
 		System.out.println("使用Map.entrySet(),使用Iterator遍历key和value");
+		Iterator<Map.Entry<String, String>> it = map.entrySet().iterator();
+		while(it.hasNext()){
+			Map.Entry<String, String> entry = it.next();
+			System.out.println("key:"+entry.getKey()+",value:"+entry.getValue());
+		}
+
+		//第三种方式，通过Map.entrySet(),遍历key和value
+		System.out.println("通过Map.entry，遍历key和value");
+		for(Map.Entry<String, String> entry :map.entrySet()){
+			System.out.println("key:"+entry.getKey()+",value:"+entry.getValue());
+		}
+
+		//第四种方式，通过Map.values遍历value,但不能遍历key
+		System.out.println("通过Map.values遍历value,但不能遍历key");
+		for(String v:map.values()){
+			System.out.println("value:"+v);
+		}
 		
 	}
 
